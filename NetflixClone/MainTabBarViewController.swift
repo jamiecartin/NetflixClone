b153bd18c7b9ad76af4d5f7fca13bfddcb9853f7
jamiecartin <jamie.cartin@icloud.com>
2023-03-02 13:39:53 -0500

    renaming

---
+++ NetflixClone/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ManTabBarViewController.swift
+//  NetflixClone
+//
+//  Created by Jamie Cartin on 3/2/23.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
