9e8a5a902932332a17b2718a0b25204b717db955
jamiecartin <jamie.cartin@icloud.com>
2023-03-03 22:57:58 -0500

    build header space and add gradient layer for blending

---
+++ NetflixClone/Views/HeroHeaderUIView.swift
@@ -0,0 +1,47 @@
+//
+//  HeroHeaderUIView.swift
+//  NetflixClone
+//
+//  Created by Jamie Cartin on 3/3/23.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
+        return imageView
+    }()
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+}
