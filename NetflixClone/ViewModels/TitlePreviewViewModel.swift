fe9138a5e6cb0c2ff285029da7428ad2fb86bf61
jamiecartin <jamie.cartin@icloud.com>
2023-03-10 12:43:26 -0500

    create title preview view

---
+++ NetflixClone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TitlePreviewViewModel.swift
+//  NetflixClone
+//
+//  Created by Jamie Cartin on 3/10/23.
+//
+
+import Foundation
+
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+}
