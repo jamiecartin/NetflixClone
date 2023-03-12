57392ff72e4ebc91e55cad04f097e8efd03b66b4
jamiecartin <jamie.cartin@icloud.com>
2023-03-11 20:09:10 -0500

    Added download view and persistence with CoreData

--- NetflixClone/Controllers/Core/DownloadsViewController.swift
+++ NetflixClone/Controllers/Core/DownloadsViewController.swift
@@ -8,12 +8,67 @@
 import UIKit
 
 class DownloadsViewController: UIViewController {
+    
+    
+    private var titles: [TitleItem] = [TitleItem]()
+
+    private let downloadedTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        title = "Downloads"
+        view.addSubview(downloadedTable)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        downloadedTable.delegate = self
+        downloadedTable.dataSource = self
+        fetchLocalStorageForDownloads()
     }
     
+    private func fetchLocalStorageForDownloads() {
+        DataPersistenceManager.shared.fetchingTitlesFromDatabase { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.downloadedTable.reloadData()
+                }
 
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        downloadedTable.frame = view.bounds
+    }
+    
 }
+
+extension DownloadsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return titles.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            let title = titles[indexPath.row]
+            cell.configure(with: TitleViewModel(titleName: (title.original_title ?? title.original_name) ?? "Unknown Title Name", posterURL: title.poster_path ?? ""))
+            return cell
+        }
+        
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 150
+        }
+        
+    }
--- NetflixClone/Views/CollectionViewTableViewCell.swift
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -56,7 +56,14 @@ class CollectionViewTableViewCell: UITableViewCell {
     }
     
     private func downloadTitleAt(indexPath: IndexPath) {
-        print("Downloading \(titles[indexPath.row].original_title)")
+        DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
+            switch result {
+            case .success():
+                print("Downloaded to Database")
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
 }
 
